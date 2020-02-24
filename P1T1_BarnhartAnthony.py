dd7ae41539d8d3dfe9e206080d0e6257f102ebc6
xtdoggx2003 <61437569+xtdoggx2003@users.noreply.github.com>
2020-02-24 17:45:43 -0500

    Add files via upload
    First Submission

---
+++ P1T1_BarnhartAnthony.py
@@ -0,0 +1,12 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ("This is a test of IDLE.")
+This is a test of IDLE.
+>>> print (This is a test of IDLE.)
+SyntaxError: invalid syntax
+>>> print ("This is a test of IDLE."
+
+
+       )
+This is a test of IDLE.
+>>> 
